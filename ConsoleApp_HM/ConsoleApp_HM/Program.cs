5bc47330a6447685d6b3ec2ff5b8572dfd6e8bd2
Vladyslav Varvashenko <vladyslav.varvashenko@stryker.com>
2023-06-24 13:11:19 +0300

    Add code.

--- ConsoleApp_HM/ConsoleApp_HM/Program.cs
+++ ConsoleApp_HM/ConsoleApp_HM/Program.cs
@@ -4,6 +4,9 @@ internal class Program
 {
     static void Main(string[] args)
     {
+        Console.Write("Hello, After Merge!");
+        Console.Write("Hello, World!");
+        Console.Write("Hello, World!");
         Console.Write("Hello, World!");
         Console.Write("Hello, World!");
         Console.Write("Hello, World!");
